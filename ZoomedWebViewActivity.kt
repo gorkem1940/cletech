e8ea1ee7d8eb09e360e5bf4b1e0d1846b0431a47
Umit Gorkem Arslaner <113816295+gorkem1940@users.noreply.github.com>
2025-04-10 17:57:03 +0300

    ZoomedWebViewActivity.kt

---
+++ ZoomedWebViewActivity.kt
@@ -0,0 +1,33 @@
+package com.example.playeropener
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class ZoomedWebViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            builtInZoomControls = true
+            displayZoomControls = false
+            setSupportZoom(true)
+        }
+
+        webView.webViewClient = WebViewClient()
+        webView.setInitialScale(125) // %125 zoom
+        webView.loadUrl("https://cletech.com.tr/en")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Hafızayı temizle
+        (findViewById<WebView>(android.R.id.content)).destroy()
+    }
+}
