ebbeb2e704526c9c67741191180463e803e1f306
Gorkem Arslaner <113816295+gorkem1940@users.noreply.github.com>
2025-06-02 10:31:56 +0300

    ZoomedWebViewActivity.kt

--- ZoomedWebViewActivity.kt
+++ ZoomedWebViewActivity.kt
@@ -7,27 +7,27 @@ import androidx.appcompat.app.AppCompatActivity
 
 class ZoomedWebViewActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+override fun onCreate(savedInstanceState: Bundle?) {
+super.onCreate(savedInstanceState)
 
-        val webView = WebView(this)
-        setContentView(webView)
+val webView = WebView(this)
+setContentView(webView)
 
-        webView.settings.apply {
-            javaScriptEnabled = true
-            builtInZoomControls = true
-            displayZoomControls = false
-            setSupportZoom(true)
-        }
+webView.settings.apply {
+javaScriptEnabled = true
+builtInZoomControls = true
+displayZoomControls = false
+setSupportZoom(true)
+}
 
-        webView.webViewClient = WebViewClient()
-        webView.setInitialScale(125) // %125 zoom
-        webView.loadUrl("https://webnav.poilabs.com/?placeId=35cc7eba-1187-39e1-0b54-5bd017227c3f&language=en")
-    }
+webView.webViewClient = WebViewClient()
+webView.setInitialScale(125) // %125 zoom
+webView.loadUrl("https://webnav.poilabs.com/?placeId=673c7846-ea6d-4dfb-b8a2-d32f49493167")
+}
 
-    override fun onDestroy() {
-        super.onDestroy()
-        //Hafızayı temizle
-        (findViewById<WebView>(android.R.id.content)).destroy()
-    }
+override fun onDestroy() {
+super.onDestroy()
+//Hafızayı temizle
+(findViewById<WebView>(android.R.id.content)).destroy()
+}
 }
