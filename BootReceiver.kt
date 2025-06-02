1730540649e09b6dc6c04b94235e616a4c208662
Gorkem Arslaner <113816295+gorkem1940@users.noreply.github.com>
2025-06-02 10:31:10 +0300

    BootReceiver.kt

--- BootReceiver.kt
+++ BootReceiver.kt
@@ -5,12 +5,12 @@ import android.content.Context
 import android.content.Intent
 
 class BootReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
-            val activityIntent = Intent(context, MainActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            }
-            context?.startActivity(activityIntent)
-        }
-    }
+override fun onReceive(context: Context?, intent: Intent?) {
+if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+val activityIntent = Intent(context, MainActivity::class.java).apply {
+addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+}
+context?.startActivity(activityIntent)
+}
+}
 }
